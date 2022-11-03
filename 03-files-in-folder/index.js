5ee05e930442b09a82f8e35997620b036b49accf
ich_kirich <99396626+ich-kirich@users.noreply.github.com>
2022-11-03 18:50:37 +0300

    feat: complete the task 03 files in folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,25 @@
+import { readdir } from 'node:fs/promises';
+import { resolve, join, extname } from 'path';
+import { stat } from 'node:fs';
+
+var filePath = resolve('secret-folder'); 
+filePath = join('03-files-in-folder', 'secret-folder')
+
+try {
+  const files = await readdir(filePath,{ withFileTypes: true });
+  for (const file of files){
+    if(file.isFile()){
+      let path = resolve(file.name); 
+      path = join('03-files-in-folder', 'secret-folder', file.name)
+      let fileSize;
+      let fileName = file.name.split('.')[0];
+      let fileType = extname(file.name);
+      stat(path, (err, stats) => {
+        fileSize = stats.size
+        console.log(fileName + " - " + fileType.slice(1) + " - " + fileSize + " bytes")
+      });
+    }
+  }
+} catch (err) {
+  console.error(err);
+}
