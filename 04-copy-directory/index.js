f85c8ea1bce310a3a6288229e44c3f39fa9e6ecf
ich_kirich <99396626+ich-kirich@users.noreply.github.com>
2022-11-04 12:19:18 +0300

    feat: complete the task 04 copy directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,45 @@
+import { readdir, mkdir } from 'node:fs/promises';
+import { resolve, join } from 'path';
+import { copyFile } from 'node:fs';
+import fs from "node:fs/promises";
+import path from "node:path";
 
+async function copyDir(){
+
+  var filePath = resolve('files'); 
+  filePath = join('04-copy-directory', 'files')
+
+  try {
+    const projectFolder = new URL('files-copy/', import.meta.url);
+    const createDir = await mkdir(projectFolder, { recursive: true });
+    console.log("Folder created!")
+    for (const file of await fs.readdir('04-copy-directory/files-copy')) {
+      await fs.unlink(path.join('04-copy-directory/files-copy', file));
+    }
+  }  
+  catch (err) {
+    console.error(err.message);
+  } // очищение и создание папки
+
+  try{
+    const files = await readdir(filePath,{ withFileTypes: true });
+    for (const file of files){
+      if(file.isFile()){
+        let path = resolve(file.name); 
+        path = join('04-copy-directory', 'files', file.name)
+        let newPath = resolve(file.name);
+        newPath = join('04-copy-directory', 'files-copy', file.name)
+        copyFile(path, newPath, (err) => {
+          if(err) throw err; // не удалось скопировать файл
+          console.log('File copied successfully');
+        });
+      }
+    }
+  }
+  catch (err) {
+    console.error(err);
+  }
+  
+}
+
+copyDir();
